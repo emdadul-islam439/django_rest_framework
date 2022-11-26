4b75306e2543e4cb6179d8f026eda3c8263bda5a
Emdadul_Islam <emdadulislam439@gmail.com>
2022-11-26 07:31:18 +0600

    created 'movie-list' and 'movie-details' urls and according views

--- watchmate/watchlist_app/views.py
+++ watchmate/watchlist_app/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
+from .models import Movie
 
 # Create your views here.
+def movie_list(request):
+    movies = Movie.objects.all()
+    data = {
+        'movies': list(movies.values())
+    }
+    print(data)
+    
+    return JsonResponse(data, safe=False)
+
+
+def movie_details(request, pk):
+    movie = Movie.objects.get(pk=pk)
+    data = {
+        'name': movie.name,
+        'description': movie.description,
+        'active': movie.active,
+    }
+    print(data)
+    
+    return JsonResponse(data, safe=False)
