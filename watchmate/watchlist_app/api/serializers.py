8215310ecf9e3ef4108541952e804ade0bea2f05
Emdadul_Islam <emdadulislam439@gmail.com>
2022-12-03 07:17:24 +0600

    added 'Review' model with admin-registration and serializer creation (url-creation, APIView-creation is still remaining...)

--- watchmate/watchlist_app/api/serializers.py
+++ watchmate/watchlist_app/api/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
 
-from ..models import WatchList, StreamPlatform
+from ..models import WatchList, StreamPlatform, Review
+
+
+class ReviewSerializer(serializers.Serializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
 
 
 class WatchListSerializer(serializers.ModelSerializer):
+    reviews = ReviewSerializer(many=True, read_only=True)
     class Meta:
         model = WatchList
         fields = '__all__' 
