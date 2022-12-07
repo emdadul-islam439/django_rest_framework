b8190165588fb8b7b43c04e8f95d8fc11b6dbb3d
Emdadul_Islam <emdadulislam439@gmail.com>
2022-12-08 02:41:11 +0600

    added code for automatically generating 'auth-token' after successfully registering an user

--- watchmate/user_app/api/views.py
+++ watchmate/user_app/api/views.py
@@ -1,14 +1,25 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
 
 from .serializers import RegistrationSerializer
+from user_app import models
 
 @api_view(['POST',])
 def registration_view(request):
     if request.method == 'POST':
         serializer = RegistrationSerializer(data=request.data)
+        data = {}
+        
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data) 
+            account = serializer.save()
+            token = Token.objects.get(user=account).key
+            
+            data['Response'] = "User created successfully!"
+            data['username'] = account.username
+            data['email'] = account.email
+            data['token'] = token
         else:
-            return Response(serializer.errors)
+            data = serializer.errors
+        
+        return Response(data)
